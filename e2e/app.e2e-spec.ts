a813f84e42bcd695d68a233044aeaa2720f123e0
angular-cli <angular-cli@angular.io>
2016-10-15 10:47:18 -0600

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MashupPage } from './app.po';
+
+describe('mashup App', function() {
+  let page: MashupPage;
+
+  beforeEach(() => {
+    page = new MashupPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
